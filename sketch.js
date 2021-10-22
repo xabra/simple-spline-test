e3b6a5d3723886ef3e39cab07540a30006a01f5a
Adam Brailove <adam.brailove@gmail.com>
2021-10-22 12:31:23 -0700

    vectorized

--- sketch.js
+++ sketch.js
@@ -1,29 +1,39 @@
-var cWidth = 800;
+var cWidth = 1000;
 var cHeight = 800;
+let o, m, v1, v2, v3, v4;
 
 function setup() {
   createCanvas(cWidth, cHeight);
-  background(240);
+  o = createVector(cWidth / 2, cHeight / 2);
+
+  v1 = createVector(0, 0);
+  v2 = createVector(50, 0);
+  v3 = createVector(50, 50);
+  v4 = createVector(0, 50);
 }
 
 function draw() {
+  background(200);
   noFill();
-  translate(cWidth / 2, cHeight / 2);
-  scale(2.0, -2.0);
+  translate(o.x, o.y);
+  //scale(2.0, -2.0);
+
+  m = createVector(mouseX, mouseY);
+  v2 = m.sub(o); //translate mouse posn into screen centered coordinates
 
   beginShape();
   strokeWeight(2);
   stroke("rgb(100%,0%,100%)");
-  vertex(0, 0);
-  bezierVertex(80, 0, 80, 75, 0, 75);
+  vertex(v1.x, v1.y);
+  bezierVertex(v2.x, v2.y, v3.x, v3.y, v4.x, v4.y);
   endShape();
 
   beginShape();
   strokeWeight(5);
   stroke("rgb(100%,0%,10%)");
-  point(0, 0);
-  point(80, 0);
-  point(80, 75);
-  point(0, 75);
+  point(v1.x, v1.y);
+  point(v2.x, v2.y);
+  point(v3.x, v3.y);
+  point(v4.x, v4.y);
   endShape();
 }
